54c46fcc79088c2fca52bbde7af248b69ebfc6f0
sean <stereo720130@gmial.com>
2020-12-29 08:26:40 +0800

    add bert

---
+++ 8/code/fox_proxy.py
@@ -0,0 +1,6 @@
+import os
+os.environ['http_proxy'] = "http://F1221528:EZ99GTSB...@10.191.131.15:3128"
+os.environ['https_proxy'] = "http://F1221528:EZ99GTSB...@10.191.131.15:3128"
+#test
+#import requests
+#requests.get("http://www.google.com.tw")
